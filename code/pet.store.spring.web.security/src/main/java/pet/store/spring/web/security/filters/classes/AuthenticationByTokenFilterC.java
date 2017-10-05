8ba5e5d771bd3cb4fcdb62b61bef9bb5c3a900bd
orbartal <Or.Bartal@intelliware.ca>
2017-10-05 13:39:54 -0400

    Use AuthenticationCredentialsNotFoundException.

--- code/pet.store.spring.web.security/src/main/java/pet/store/spring/web/security/filters/classes/AuthenticationByTokenFilterC.java
+++ code/pet.store.spring.web.security/src/main/java/pet/store/spring/web/security/filters/classes/AuthenticationByTokenFilterC.java
@@ -4,12 +4,12 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 import pet.store.spring.web.security.converters.interfaces.ConversionServiceSecurityI;
+import pet.store.spring.web.security.exceptions.InvalidAuthenticationServletException;
 import pet.store.spring.web.security.filters.interfaces.AuthenticationFilterI;
 
 public 	class AuthenticationByTokenFilterC 
@@ -31,7 +31,7 @@ public 	class AuthenticationByTokenFilterC
 			SecurityContextHolder.getContext().setAuthentication(auth2);
 			chain.doFilter(request, response);
 		} catch (Exception e) {
-			throw new AuthenticationCredentialsNotFoundException (e.getMessage());
+			throw new InvalidAuthenticationServletException (e);
 		} 
 	}
 }
