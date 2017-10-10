9a9c4faaf695374cd1620c64ef830e1d14ac74ae
orbartal <Or.Bartal@intelliware.ca>
2017-10-10 15:35:42 -0400

    Fix bug with STRING_TOKEN_ANONYMOUS = null;

--- code/pet.store.spring.main/src/test/java/pet/store/spring/main/test/system/hello/HelloWorldSystemTestC.java
+++ code/pet.store.spring.main/src/test/java/pet/store/spring/main/test/system/hello/HelloWorldSystemTestC.java
@@ -2,12 +2,13 @@ package pet.store.spring.main.test.system.hello;
 
 import org.junit.Test;
 import org.junit.Before;
+import static org.junit.Assert.assertTrue;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import pet.store.spring.main.test.system.AbstractSystemTestC;
-import pet.store.spring.main.test.system.utiles.TokensExamplesI;
+import pet.store.spring.web.security.tokens.TokensExamplesI;
 import pet.store.spring.web.swagger.controllers.interfaces.HelloWorldControllerI;
-import static org.assertj.core.api.Assertions.assertThat;
+
 
 /**System Test for the HelloWorld api.*/
 public class HelloWorldSystemTestC extends AbstractSystemTestC {
@@ -23,28 +24,28 @@ public class HelloWorldSystemTestC extends AbstractSystemTestC {
 
 	@Test
 	public void helloByInvalid() {
-		ResponseEntity<String> response = getHelloFromServer (TokensExamplesI.INVALID);
-		assertThat(org.springframework.http.HttpStatus.UNAUTHORIZED.equals(response.getStatusCode()));
+		ResponseEntity<String> response = getHelloFromServer (TokensExamplesI.STRING_TOKEN_INVALID);
+		assertTrue(org.springframework.http.HttpStatus.UNAUTHORIZED.equals(response.getStatusCode()));
 	}
 	
 	@Test
 	public void helloByAnonymous() {
-		ResponseEntity<String> response = getHelloFromServer (TokensExamplesI.ANONYMOUS);
-		assertThat(org.springframework.http.HttpStatus.UNAUTHORIZED.equals(response.getStatusCode()));
+		ResponseEntity<String> response = getHelloFromServer (TokensExamplesI.STRING_TOKEN_ANONYMOUS);
+		assertTrue(org.springframework.http.HttpStatus.UNAUTHORIZED.equals(response.getStatusCode()));
 	}
 
 	@Test
 	public void helloByLimited() {
-		ResponseEntity<String> response = getHelloFromServer (TokensExamplesI.LIMITED);
-		assertThat(org.springframework.http.HttpStatus.OK.equals(response.getStatusCode()));
-		assertThat(response.getBody()).isEqualTo(HelloWorldControllerI.HELLO_RESULT);
+		ResponseEntity<String> response = getHelloFromServer (TokensExamplesI.STRING_TOKEN_LIMITED);
+		assertTrue(org.springframework.http.HttpStatus.OK.equals(response.getStatusCode()));
+		assertTrue(response.getBody().equals(HelloWorldControllerI.HELLO_RESULT));
 	}
 
 	@Test
 	public void helloByAdmin() {
-		ResponseEntity<String> response = getHelloFromServer (TokensExamplesI.ADMIN);
-		assertThat(org.springframework.http.HttpStatus.OK.equals(response.getStatusCode()));
-		assertThat(response.getBody()).isEqualTo(HelloWorldControllerI.HELLO_RESULT);
+		ResponseEntity<String> response = getHelloFromServer (TokensExamplesI.STRING_TOKEN_ADMIN);
+		assertTrue(org.springframework.http.HttpStatus.OK.equals(response.getStatusCode()));
+		assertTrue(response.getBody().equals(HelloWorldControllerI.HELLO_RESULT));
 	}
 	
 }
--- code/pet.store.spring.web.security/src/test/java/pet/store/spring/web/security/tokens/TokensExamplesI.java
+++ code/pet.store.spring.web.security/src/test/java/pet/store/spring/web/security/tokens/TokensExamplesI.java
@@ -2,7 +2,7 @@ package pet.store.spring.web.security.tokens;
 
 public interface TokensExamplesI {
 	final static String STRING_TOKEN_INVALID  =  "blabla";
-	final static String STRING_TOKEN_ANONYMOUS  =  null;
+	final static String STRING_TOKEN_ANONYMOUS  =  "";
 	final static String STRING_TOKEN_LIMITED =  "{\"role\":\"limit\",\"credentials\":\"limit\",\"name\":\"limit\"}";
 	final static String STRING_TOKEN_ADMIN =  "{\"role\":\"admin\",\"credentials\":\"admin\",\"name\":\"admin\"}";
 	//final static String JSON_TOKEN_LIMITED =  "{\"role\":\"limited\",\"credentials\":\"pass\",\"name\":\"limited\"}";
